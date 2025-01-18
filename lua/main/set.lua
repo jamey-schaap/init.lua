bf0215da109595bcf4a27821ba26a532c53a86cc
jamey-schaap <jameyschaap@proton.me>
2025-01-18 22:42:49 +0100

    Added highlighted yank

--- lua/main/set.lua
+++ lua/main/set.lua
@@ -30,3 +30,12 @@ vim.opt.colorcolumn = "80"
 
 vim.g.mapleader = " "
 vim.g.terminal_emulator="pwsh"
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  group = vim.api.nvim_create_augroup('highlight_yank', {}),
+  desc = 'Hightlight selection on yank',
+  pattern = '*',
+  callback = function()
+    vim.highlight.on_yank { higroup = 'IncSearch', timeout = 200 }
+  end,
+})
